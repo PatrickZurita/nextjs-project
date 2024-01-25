43627697b77aaae25fe76f726f5bd1dc4c078ee2
pTrick <71539616+PatrickZurita@users.noreply.github.com>
2024-01-24 23:08:32 -0500

    Instant Loading States - Suspense
    In addition to loading.js, you can also manually create Suspense Boundaries for your own UI components. The App Router supports streaming with Suspense for both Node.js and Edge runtimes.

--- app/posts/[postId]/page.jsx
+++ app/posts/[postId]/page.jsx
@@ -1,3 +1,5 @@
+import Posts from '../page'
+import { Suspense } from 'react';
 
 async function loadPost(id) {
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
@@ -12,6 +14,11 @@ export default async function Page({params}) {
     <div>
         <h1>{post.id}. {post.title}</h1>
         <p>{post.body}</p>
+        <hr/>
+        <h3>Otras Publicaciones</h3>
+        <Suspense fallback={<div>Cargando Publicaciones...</div>}>
+          <Posts/>
+        </Suspense>
     </div>
   )
 }
